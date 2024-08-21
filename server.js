50adbe85697021f0302831b2d4b1d8b927d4f256
Insomnia <wuzhaoliang@echo.tech>
2024-08-21 20:22:20 +0800

    mongodb new api

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { ObjectId } = require('mongodb');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
 const app = express();
@@ -10,12 +11,26 @@ const client = new MongoClient(uri);
 app.use(cors());
 app.use(express.json());
 
-app.get('/v1/getAllCollections', async (req, res) => {
+app.get('/v1/getCollectionCount', async (req, res) => {
     try {
         await client.connect();
+        const { collectionName, queryParams = {} } = req.query;
+
+        let query = queryParams;
+        if (typeof queryParams === 'string') {
+            try {
+                query = JSON.parse(queryParams);
+            } catch (error) {
+                return res.status(400).json({ error: 'Invalid queryParams format' });
+            }
+        }
+
         const database = client.db('crawler');
-        const collections = await database.listCollections().toArray();
-        res.json(collections.map(c => c.name));
+        const collection = database.collection(collectionName);
+
+        const count = await collection.countDocuments(query);
+
+        res.json({ collectionName, documentCount: count });
     } catch (error) {
         res.status(500).json({ error: error.message });
     } finally {
@@ -24,10 +39,19 @@ app.get('/v1/getAllCollections', async (req, res) => {
 });
 
 app.get('/v1/dataQuery', async (req, res) => {
-    const { collectionName, limit = 20, sort = 'desc' } = req.query;
+    const { collectionName, limit = 20, skipCount = 0, sort = 'desc', queryParams = '{}' } = req.query;
 
     if (!collectionName) {
         return res.status(400).json({ error: 'Collection name is required' });
+    }
+
+    let query = queryParams;
+    if (typeof queryParams === 'string') {
+        try {
+            query = JSON.parse(queryParams);
+        } catch (error) {
+            return res.status(400).json({ error: 'Invalid queryParams format' });
+        }
     }
 
     try {
@@ -35,16 +59,45 @@ app.get('/v1/dataQuery', async (req, res) => {
         const database = client.db('crawler');
         const collection = database.collection(collectionName);
 
-        // 根据排序参数设置排序规则
         const sortOrder = sort === 'asc' ? 1 : -1;
 
-        // 查询数据库，按指定数量和排序返回结果
-        const games = await collection.find()
-            .sort({ _id: sortOrder })  // 按 _id 字段排序，最新的数据在前
+        const games = await collection.find(query)
+            .sort({ _id: sortOrder })
+            .skip(parseInt(skipCount))
             .limit(parseInt(limit))
             .toArray();
 
         res.json(games);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    } finally {
+        await client.close();
+    }
+});
+
+app.post('/v1/syncDBStatus', async (req, res) => {
+    const { collectionName, id } = req.body;
+    if (!collectionName || !id) {
+        return res.status(400).json({ error: 'Collection name and id are required' });
+    }
+
+    try {
+        await client.connect();
+        const database = client.db('crawler');
+        const collection = database.collection(collectionName);
+
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+            $set: { 'db_status': "DB_SYNC" },
+        };
+
+        const result = await collection.updateOne(filter, updateDoc);
+
+        if (result.modifiedCount === 0) {
+            return res.status(404).json({ error: 'Document not found or db_status not modified' });
+        }
+
+        res.status(200).json({ message: 'Property updated successfully', result, status: 200 });
     } catch (error) {
         res.status(500).json({ error: error.message });
     } finally {
